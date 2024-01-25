3353c0664a9525e0c0fa177fe4b18de87c62f530
Daniele Ranzani <daniele.ranzani@itsincom.it>
2024-01-25 16:55:24 +0100

    file appena creato

---
+++ Sudoku/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Let's play Sudoku!");
+
+            Console.ReadKey();
+
+        }
+    }
+}
