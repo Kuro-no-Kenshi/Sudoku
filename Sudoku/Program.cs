6fe2645f4f0ca0054d4f791791d59a5999186585
Daniele Ranzani <daniele.ranzani@itsincom.it>
2024-02-01 14:54:11 +0100

    inserimento numeri random

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -43,5 +43,29 @@ namespace Sudoku
                 Console.WriteLine();    
             }
         }
+
+        static void AddRandomNumbers()
+        {
+            Random random = new Random();
+
+            // Numero di numeri casuali da inserire
+            int randomNumbersCount = 15;
+
+            for (int i = 0; i < randomNumbersCount; i++)
+            {
+                int randomRow = random.Next(0, 9);
+                int randomCol = random.Next(0, 9);
+                int randomValue = random.Next(1, 10);
+
+                // Verifica che la cella sia vuota prima di inserire un numero casuale
+                while (board[randomRow, randomCol] != 0)
+                {
+                    randomRow = random.Next(0, 9);
+                    randomCol = random.Next(0, 9);
+                }
+
+                board[randomRow, randomCol] = randomValue;
+            }
+        }
     }
 }
