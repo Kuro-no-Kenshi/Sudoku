4fab987f9a71bf675eb3b7f7de0d445b1be766a4
Daniele Ranzani <daniele.ranzani@itsincom.it>
2024-02-01 14:56:52 +0100

    completamento degli ultimi metodi

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -67,5 +67,47 @@ namespace Sudoku
                 board[randomRow, randomCol] = randomValue;
             }
         }
+        static bool IsValidMove(int row, int col, int num)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (board[row, i] == num || board[i, col] == num)
+                    return false;
+            }
+
+            int startRow = row - row % 3;
+            int startCol = col - col % 3;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i + startRow, j + startCol] == num)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        static bool IsSudokuSolved()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (board[i, j] == 0)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        static void SolveSudoku()
+        {
+            Console.WriteLine("\nSudoku risolto:");
+            PrintBoard();
+        }
+
     }
 }
